7988d32dcf0f49c7c2ec441266a49f3e68060685
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2020-04-26 10:19:43 -0500

    more work on Safari hack

---
+++ holy-grail.js
@@ -0,0 +1,51 @@
+let nav;
+let navOpen = false;
+let navWidth = 0;
+
+function toggleMenu() {
+  navOpen = !navOpen;
+  nav.style.left = navOpen ? 0 : '-' + navWidth + 'px';
+  nav;
+}
+
+window.onload = () => {
+  nav = document.querySelector('nav');
+  navWidth = nav.getBoundingClientRect().width;
+  // Set a CSS variable so the nav width is available in holy-grail.css.
+  nav.style.setProperty('--width', navWidth);
+
+  // Maybe the nav start out hidden in mobile view.
+  nav.style.left = `-${navWidth}px`;
+
+  let selectedAnchor;
+
+  const anchors = nav.querySelectorAll('a');
+  Array.from(anchors).forEach(anchor => {
+    anchor.addEventListener('click', () => {
+      // If there was a previously selected anchor, unselect it.
+      if (selectedAnchor) selectedAnchor.classList.toggle('selected');
+
+      // Select the new anchor.
+      anchor.classList.toggle('selected');
+      selectedAnchor = anchor;
+
+      // Close the nav in mobile view.
+      toggleMenu();
+    });
+  });
+
+  // Safari hack:
+  const {userAgent} = navigator;
+  const isSafari =
+    userAgent.includes('Safari/') &&
+    !userAgent.includes('Chrome/') &&
+    !userAgent.includes('Chromium/');
+  if (isSafari) {
+    // Set nav CSS property max-height so nav scrolling works.
+    header = document.querySelector('header');
+    headerHeight = nav.getBoundingClientRect().height;
+    footer = document.querySelector('footer');
+    footerHeight = nav.getBoundingClientRect().height;
+    nav.style.maxHeight = `calc(100vh - ${headerHeight + footerHeight})`;
+  }
+};
